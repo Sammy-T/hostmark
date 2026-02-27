7c7d775aaa3957a841141bf6d36be96ae73ebdb2
Sammy-T <cyam9019@gmail.com>
2026-02-26 19:56:24 -0500

    Serve site via go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sammy-t/hostmark
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func main() {
+	startDevServer("pnpm")
+	http.Handle("/", createDevHandler())
+
+	addr := ":3000"
+
+	log.Printf("Serving hostmark on %v", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func startDevServer(pkgManager string) {
+	log.Println("Starting Vite dev server...")
+
+	cwDir, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Current working directory: %v", err)
+	}
+
+	cmdDir := filepath.Join(cwDir, "web")
+
+	cmdInst := exec.Command(pkgManager, "i")
+	cmdInst.Dir = cmdDir
+	cmdInst.Stdout = os.Stdout
+	cmdInst.Stderr = os.Stderr
+
+	// Run install and await
+	if err = cmdInst.Run(); err != nil {
+		if pkgManager != "npm" {
+			log.Printf("%v install failed: %v.\nFalling back to npm...", pkgManager, err)
+
+			startDevServer("npm")
+			return
+		}
+
+		log.Fatalf("%v install: %v", pkgManager, err)
+	}
+
+	cmd := exec.Command(pkgManager, "run", "dev")
+	cmd.Dir = cmdDir
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// Run dev without awaiting
+	if err = cmd.Start(); err != nil {
+		log.Fatalf("Dev server: %v", err)
+	}
+
+	log.Printf("Dev server running as pid %v", cmd.Process.Pid)
+}
+
+func createDevHandler() http.Handler {
+	devUrl, err := url.Parse("http://localhost:5173")
+	if err != nil {
+		log.Fatalf("Dev url: %v", err)
+	}
+
+	errHandler := func(w http.ResponseWriter, r *http.Request, err error) {
+		log.Print(err)
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusBadGateway)
+
+		w.Write([]byte("Error: " + err.Error()))
+	}
+
+	rewrite := func(r *httputil.ProxyRequest) {
+		// log.Printf("proxy req path %q", r.In.URL.String())
+
+		if r.In.URL.Path == "/auth" {
+			// Can't directly set r.Out.URL here for some reason
+			// even though it's a pointer
+			r.Out.URL.Scheme = devUrl.Scheme
+			r.Out.URL.Host = devUrl.Host
+			r.Out.URL.Path = "/login"
+
+			log.Printf("%q => %q %q", r.In.URL.String(), r.Out.URL.String(), r.Out.URL.Path)
+			return
+		}
+
+		r.SetURL(devUrl)
+		// log.Printf("=> %q", r.Out.URL.String())
+	}
+
+	proxy := &httputil.ReverseProxy{
+		Rewrite:      rewrite,
+		ErrorHandler: errHandler,
+	}
+
+	// proxy := httputil.NewSingleHostReverseProxy(devUrl)
+	// proxy.ErrorHandler = errHandler
+
+	return proxy
+}
