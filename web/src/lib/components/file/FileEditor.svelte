9be647e9571bd26053433b133f5017ec267d5612
Sammy-T <cyam9019@gmail.com>
2026-03-12 20:05:13 -0400

    Toggle editing mode

--- web/src/lib/components/file/FileEditor.svelte
+++ web/src/lib/components/file/FileEditor.svelte
@@ -2,9 +2,30 @@
     import { page } from '$app/state';
 
     let content = $derived(page.data.content);
+    let editing = $state(false);
+
+    let lastClick = 0;
+
+    function onDoubleClick() {
+        editing = !editing;
+    }
+
+    function onClick() {
+        const clickTime = new Date().getTime();
+
+        if(clickTime - lastClick < 250) onDoubleClick();
+
+        lastClick = clickTime;
+    }
 </script>
 
-<section>{@html content?.html}</section>
+{#if editing}
+    <textarea onclick={onClick}>{content?.markdown}</textarea>
+{:else}
+    <!-- svelte-ignore a11y_click_events_have_key_events -->
+    <!-- svelte-ignore a11y_no_static_element_interactions -->
+    <section onclick={onClick}>{@html content?.html}</section>
+{/if}
 
 <style>
     section {
@@ -12,5 +33,11 @@
         padding: 0.5rem;
         margin: 0;
         overflow: auto;
+    }
+
+    textarea {
+        flex-grow: 1;
+        padding: 0.5rem;
+        margin: 0;
     }
 </style>
