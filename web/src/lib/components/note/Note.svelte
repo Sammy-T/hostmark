584651a01112658b0f65b999edff2dd25feebfbc
Sammy-T <cyam9019@gmail.com>
2026-04-03 03:59:10 -0400

    Load note data

--- web/src/lib/components/note/Note.svelte
+++ web/src/lib/components/note/Note.svelte
@@ -1,19 +1,36 @@
 <script>
+    import icPublic from '$lib/assets/world-map.svg?raw';
+    import icProtected from '$lib/assets/users.svg?raw';
+    import icPrivate from '$lib/assets/lock.svg?raw';
     import icMore from '$lib/assets/dots-vertical.svg?raw';
-    /**
-     * @typedef {Object} Props
-     * @property {string} content
-     */
-    let { content } = $props();
+
+    let { note } = $props();
 </script>
 
 <article>
     <header>
-        <small>time</small>
-        <button>{@html icMore}</button>
+        <small>{new Intl.DateTimeFormat().format(new Date(note.created_at))}</small>
+        
+        <div class="more">
+            {#if note.visibility === 'public'}
+                {@html icPublic}
+            {:else if note.visibility === 'protected'}
+                {@html icProtected}
+            {:else if note.visibility === 'private'}
+                {@html icPrivate}
+            {/if}
+            
+            <button>{@html icMore}</button> <!-- TODO: add menu -->
+        </div>
     </header>
 
-    {content}
+    {note.content}
+    
+    <div class="tags">
+        {#each note.tags as tag (tag.name)}
+            <a href="#">{tag.name}</a>
+        {/each}
+    </div>
 </article>
 
 <style>
@@ -29,12 +46,26 @@
                 color: var(--pico-muted-color);
             }
 
-            & > button {
+            & button {
                 padding: 0;
                 background-color: transparent;
                 border: none;
                 color: oklch(from var(--pico-contrast) l c h / 0.5);
             }
+
+            .more > :global(svg) {
+                color: oklch(from var(--pico-contrast) l c h / 0.5);
+            }
+        }
+    }
+
+    .tags {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 0.4rem;
+
+        & a {
+            text-decoration: underline;
         }
     }
 </style>
--- web/src/lib/components/note/NoteBrowser.svelte
+++ web/src/lib/components/note/NoteBrowser.svelte
@@ -1,18 +1,25 @@
 <script>
     import NoteHeader from './NoteHeader.svelte';
     import Note from './Note.svelte';
+    import { page } from '$app/state';
+
+    let notes = $derived(page.data.notes);
+
+    $effect(() => {
+        console.log($state.snapshot(notes));
+    });
 </script>
 
 <section>
     <NoteHeader />
 
-    {#each { length: 50 } as _, i }
-    <Note content={`note-${i+1}`} />
+    {#each notes as note (note.id)}
+        <Note {note} />
+    {:else}
+        <div class="empty-view">
+            <h3>No notes found.</h3>
+        </div>
     {/each}
-
-    <!-- <div class="empty-view">
-        <h3>No notes found.</h3>
-    </div> -->
 </section>
 
 <style>
@@ -25,10 +32,10 @@
         gap: 0.75rem;
         overflow: auto;
 
-        /* & .empty-view {
+        & .empty-view {
             flex-grow: 1;
             align-content: center;
             text-align: center;
-        } */
+        }
     }
 </style>
