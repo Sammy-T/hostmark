b251a3145ad70a11ee1c12ae72f307cbe827acb6
Sammy-T <cyam9019@gmail.com>
2026-04-11 03:12:03 -0400

    Load root readme file

--- web/src/lib/hello.md
+++
@@ -1,8 +0,0 @@
-# Welcome to SvelteKit + Markdown
-
-This site imports markdown and converts it to HTML.
-
-## Using the following
-
-- SvelteKit <https://svelte.dev/>
-- Marked <https://marked.js.org/>
--- web/src/routes/(app)/+page.js
+++ web/src/routes/(app)/+page.js
@@ -1,12 +1,48 @@
-import mdHello from '$lib/hello.md?raw';
+import { goto } from '$app/navigation';
+import { error } from '@sveltejs/kit';
 import { marked } from 'marked';
 
-// TODO: TEMP
-export async function load({ params }) {
-    const html = await marked.parse(mdHello);
+export const ssr = false
+
+export async function load({ fetch, params }) {
+    let resp = await fetch(`/api/file/readme.md`);
+
+    switch(resp.status) {
+        case 200:
+            break;
+
+        case 400:
+            return {};
+
+        case 401:
+            const refResp = await fetch('/api/auth/refresh');
+
+            switch(refResp.status) {
+                case 200:
+                    resp = await fetch(`/api/file/readme.md`);
+                    if(!resp.ok) error(resp.status, resp.statusText);
+                    break;
+
+                case 400:
+                case 401:
+                    goto('/login');
+
+                default:
+                    const msg = await refResp.text();
+                    error(refResp.status, msg);
+            }
+            break;
+
+        default:
+            error(resp.status, resp.statusText);
+    }
+
+    const markdown = await resp.text();
+    const html = await marked.parse(markdown);
+
     return { 
         content: {
-            markdown: mdHello,
+            markdown,
             html
         }
     };
