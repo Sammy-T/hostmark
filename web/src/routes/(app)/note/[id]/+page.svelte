4d6ceecb36a93a6a19f205e0ba6ba2477c0c924f
Sammy-T <cyam9019@gmail.com>
2026-04-11 23:49:23 -0400

    Center note on individual note page

--- web/src/routes/(app)/note/[id]/+page.svelte
+++ web/src/routes/(app)/note/[id]/+page.svelte
@@ -3,13 +3,21 @@
     import { page } from '$app/state';
 </script>
 
-<div>
-    <Note note={page.data.note} />
+<div class="page">
+    <div class="note-container">
+        <Note note={page.data.note} />
+    </div>
 </div>
 
 <style>
-    div {
-        height: 100dvh;
+    .page {
+        height: 100%;
+        padding: 1rem;
         overflow: auto;
+        justify-content: center;
+    }
+
+    .note-container {
+        width: min(100%, 1000px);
     }
 </style>
